6d7de79d114c97eb84f6d5c2b0ed445f128f1be6
DanCristian4 <dan.galchis@stud.ubbcluj.ro>
2025-01-14 19:41:26 +0200

    update 14.01.2025 - validare

--- Models/Client.cs
+++ Models/Client.cs
@@ -7,7 +7,11 @@ namespace P_Galchis_Cristian.Models
         public int ID { get; set; }
         public string? Nume { get; set; }
         public string? Prenume { get; set; }
+
+       
         public string Email { get; set; }
+
+        [RegularExpression(@"^07\d{8}$", ErrorMessage = "Numarul de telefon trebuie sa inceapa cu 07 si sa contina 8 cifre.")]
         public string? Telefon { get; set; }
 
         [Display(Name = "Nume Complet")]
--- Models/Stil.cs
+++ Models/Stil.cs
@@ -1,10 +1,18 @@
-﻿namespace P_Galchis_Cristian.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P_Galchis_Cristian.Models
 {
     public class Stil
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Denumirea este obligatorie.")]
+        [StringLength(40, ErrorMessage = "Denumirea nu poate depasi {1} caractere.", MinimumLength = 3)]
         public string Denumire { get; set; }
-        public string Descriere { get; set; } 
+
+        [StringLength(500, ErrorMessage = "Descrierea nu poate depasi {1} caractere.")]
+        [MinLength(15, ErrorMessage = "Descrierea trebuie sa aiba cel putin {1} caractere.")]
+        public string Descriere { get; set; }
 
         public ICollection<Obiectiv>? Obiective { get; set; }
     }
